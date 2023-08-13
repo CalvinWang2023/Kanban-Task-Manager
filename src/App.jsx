2629233afabb41628ada0d810f46fab9ec59c542
Yanchun Wang <wang146@mcmaster.ca>
2023-08-13 05:03:48 -0400

    Build App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './components/header/Header';
+import Board from './components/board/Board';
+
+import './App.css';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    const [theme, setTheme] = useState('light');
+
+    return (
+        <div className={`App ${ theme }`}>
+            <Header />
+            
+            <Board />
+        </div>
+    )
 }
 
 export default App
