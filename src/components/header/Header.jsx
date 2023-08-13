787ccc3a3ab883fd30f14613476762a24f5c2a47
Yanchun Wang <wang146@mcmaster.ca>
2023-08-12 23:09:16 -0400

    Build Header.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,42 @@
+import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
+import './Header.css';
+
 import logo from "../../assets/logo-mobile.svg";
 import chevronUp from '../../assets/icon-chevron-up.svg';
 import chevronDown from '../../assets/icon-chevron-down.svg';
 import addTask from '../../assets/icon-add-task-mobile.svg';
 import ellipsis from '../../assets/icon-vertical-ellipsis.svg';
-import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const Header = () => {
     const isBigScreen = useMediaQuery({ query: "(min-width: 768px)" });
     const [boardModalUnfolded, setBoardModalUnfolded] = useState(true);
+    const boards = useSelector((state) => state.boards);
+    const activeBoardIndex = useSelector((state) => state.activeBoardIndex); 
+    const activeBoard = boards[activeBoardIndex];
 
     return (
        <div className="header-container">
             <header>
-                <div className="logo-container">
-                    <img src={ logo } alt="logo" />
-                    {isBigScreen && <h3 className="logo-text">kanban</h3>}
+                <div className="left">
+                    <div className="logo-container">
+                        <img src={ logo } alt="logo" />
+                        {isBigScreen && <h3 className="logo-text">kanban</h3>}
+                    </div>
+                    <div className="board-name-container">
+                        <h4>{ activeBoard.name }</h4>
+                        { !isBigScreen && <img src={ boardModalUnfolded ? chevronDown : chevronUp } onClick={ () => setBoardModalUnfolded(!boardModalUnfolded) } alt="chevron up/down" /> }
+                    </div>
                 </div>
-                <div className="board-name-container">
-                    <img src={ boardModalUnfolded ? chevronDown : chevronUp } onClick={ () => setBoardModalUnfolded(!boardModalUnfolded) } alt="chevron up/down" />
+                <div className="right">
+                    <button>   
+                        <p>{isBigScreen ? '+ Add New Task' : '+'}</p>       
+                    </button>
+                    <div className="ellipsis">
+                        <img src={ ellipsis } alt="ellipsis menu" />
+                    </div>
                 </div>
-                <button>
-                    <img src={ addTask } alt="add task button" />
-                </button>
-                <img src={ ellipsis } alt="ellipsis menu" />
             </header>
        </div> 
     )
